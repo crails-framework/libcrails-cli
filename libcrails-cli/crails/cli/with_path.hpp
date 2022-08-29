4f270b34425e7f34e11ec36ab7824cd5f8f0cc07
plaristote <michael@unetresgrossebite.com>
2022-08-29 21:24:55 +0200

    adds WithPath

---
+++ libcrails-cli/crails/cli/with_path.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <filesystem>
+
+namespace Crails
+{
+  class WithPath
+  {
+    const std::filesystem::path old_path;
+  public:
+    WithPath(std::filesystem::path new_path) : old_path(boost::filesystem::current_path())
+    {
+      std::filesystem::create_directories(new_path);
+      std::filesystem::current_path(new_path);
+    }
+
+    ~WithPath()
+    {
+      std::filesystem::current_path(old_path);
+    }
+  };
+}
