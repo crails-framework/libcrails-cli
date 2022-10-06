173fff038f875b1b5cce7e1cc359098bb06d5030
plaristote <michael@unetresgrossebite.com>
2022-10-06 16:31:16 +0200

    fix execve erasing last argument

--- libcrails-cli/crails/cli/process.cpp
+++ libcrails-cli/crails/cli/process.cpp
@@ -51,7 +51,7 @@ namespace Crails
       argv[0] = command.c_str();
       for (size_t i = 1 ; i <= arguments.size() ; ++i)
         argv[i] = arguments[i - 1].c_str();
-      argv[arguments.size()] = nullptr;
+      argv[arguments.size() + 1] = nullptr;
 #ifndef _WIN32
       return ::execve(command.c_str(), const_cast<char **const>(argv), nullptr);
 #else
